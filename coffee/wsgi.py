b1b358e2a60b4edadb1d04bb5cb801a22e6613a9
TraianAlex <victor_traian@yahoo.com>
2015-04-21 18:46:31 -0400

    second

--- coffee/wsgi.py
+++ coffee/wsgi.py
@@ -13,10 +13,10 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "coffee.settings")
 
-#from dj_static import Cling
-#application = Cling(get_wsgi_application())
+from dj_static import Cling
+application = Cling(get_wsgi_application())
 
-application = get_wsgi_application()
+#application = get_wsgi_application()
 
 #web: gunicorn coffee.wsgi --log-file -
 #web: python manage.py runserver 0.0.0.0:8000
